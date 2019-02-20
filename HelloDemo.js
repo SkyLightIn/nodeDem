19abf49009d09c39d6e1b1bc1f032a356bd967c4
Sky <357757014@qq.com>
2019-02-20 23:13:06 +0800

    First img server

--- HelloDemo.js
+++ HelloDemo.js
@@ -1,7 +1,16 @@
+// let http = require("http");
+// let server = http.createServer(function(req, res) {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end("Helle node\n");
+// });
+// server.listen(3000);
+// console.log("server is running at http://localhost:3000");
+
 let http = require("http");
-let server = http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Helle node\n");
+let fs = require("fs");
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "image/png"});
+    fs.createReadStream("./3.png").pipe(res);
 });
 server.listen(3000);
-console.log("server is running at http://localhost:3000");
+console.log("server is listen http://0.0.0.0:3000");
